7d1b7e787b54ab91455e854b9cce51f64c376f89
Benjamin Perry* <famogr.perry@gmail.com>
2024-05-02 15:34:33 -0400

    print.py is filled with techniques with Python print statements and band_name_generator.py generates the name of the user's band based off the town they live in and their favorite plural noun

---
+++ Day1/band_name_generator.py
@@ -0,0 +1,3 @@
+town = input("What's the name of the city or town you live in?\n")
+pl_noun = input("What's your favorite plural noun?\n")
+print("I see you in a band called 'The " + town + " " + pl_noun + "' in your near future!")
---
+++ Day1/print.py
@@ -0,0 +1,24 @@
+# Typical Print Statement
+print("Hello again world!")
+
+# Quotes as text
+print("Hello 'world'")
+print('Okay "world"')
+
+# Multiple lines in a single print statement
+print("Knock\nKnock\nKnock")
+
+# Concat Strings
+print("Ben" + " " + "Perry")
+
+# Debugging came from the fact that a moth flew into a computer and the programmer had to take out the electrified moth and rewire the computer for it to work
+
+# Getting input with input() function
+name = input("What is your name? ")
+
+# Can put input in print statement; Input from the console is received first, then statement is printed
+print("" + name + " is feeling " + input("User is feeling: "))
+
+# Length function for strings
+print(len("Hello"))
+
